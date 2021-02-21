ed44c56dfbf504296f6cd8be29e764522d152864
Devon Martens <devonmartens@Devons-MacBook-Pro-2.local>
2021-02-21 17:31:14 -0500

    readme done

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# SolidityContractLesson
+# Solidity Contract Lesson
+
+![Etherum](https://github.com/DevonMartens/SolidityContractLesson/blob/master/Photo/etherum.jpg?raw=true)
+
+This a lesson on writing a smart contract in Solidity. In order to understand smart contracts, you first need to know what the Ethereum blockchain is and how it works. It does not have a centralized source like a cloud platform such as Google Cloud or Amazon Web Services would. Developers run Ethereum on their machines and are paid in cryptocurrencies called ether. A cryptocurrency is essentially a digital asset. The ledger that tracks these assets is the Ethereum blockchain (there are other types of blockchain that also serve as ledgers for cryptocurrencies). The Ethereum blockchain is the database that tracks what is owed, stored, and runs smart contracts.
+
+Developers who run the Ethereum blockchain on their machine for ether are called miners. Gas is the cost necessary to perform transactions on the network based on the amount of memory your contract uses. Miners who use their computer to run the Ethereum Blockchain (virual machine) set these costs based on supply and demand. The network that all these computers are running on to mine is the Ethereum virtual machine. That is the network that the Ethereum Blockchain runs on. It is in your best interest to ensure that your program uses the least amount of memory and data to save money, or gas.
+
+A contract that runs on the Ethereum blockchain automatically executes when all the conditions are met. Since, digital assets can be transfer and contracts can be written this is valueable because it eliminates the need for a lawyer to write a contract and a bank to transfer money. 
+
+![Etherum](https://github.com/DevonMartens/SolidityContractLesson/blob/master/Photo/Explained.jpg?raw=true)
+
+# This Lesson is on Writing Contracts in Solidity
+
+In the Solution Folder there is a written smart contract and a step by step guid to writing it and it is doing. 
+
+If you want to try it on your own first there is a starter code file and instructions in the starter code on what to do. 
+
+In the lesson folder there is a guide to writing in solidity which talks about syntax, the Ethereum blockchain is better explained there as well, and the solution at the end of the lesson is explained.
+
+# Key take aways from this Lesson:
+
+1.	Why blockchain developers are so valuable
+2.	Ethereum blockchain fundamentals
+-   	How the Network works
+-   	Mining and what it is
+-   	Gas
+-   	Ether
+3.	Smart Contracts and how they work!
+4.	Solidity fundamentals including:
+-   	Declare the version of Solidity for the compiler to use with pragma.
+-   	Create a contract.
+-   	Events (declare and emit)
+-   	Functions
+-   	Variables
+-   	Returns
+-   	If/else statements
+-   	For loops
+-   	Mapping
+5.	The remix IDE
+-   	How to write/compile a contract on remix
+
+# The next lesson will be about:
+In our next lesson we will learn how to deploy contracts and create and use real addresses for contracts on the Ethereum network.
+
+
+
---
+++ starter_code/start.sol
@@ -0,0 +1,105 @@
+// add pragma to specify Solidity version
+ 
+//TO DO: Name the contract it begins here
+ 
+/* the asset it is safe to say that we will need a quantity/price and address to be sent and
+an identifier (struct)*/
+ 
+  	// The transaction structure begins here
+  	/* a transaction needs a price/quantity also, we should track when it occurred, if it was successful
+  	and the buyer and seller*/
+   
+	// let's use int or numbers to count our transactions and assets (create variables)
+  
+	// mapping for each struct so we can access our transaction and asset
+ 
+//TO DO: write your first function
+ 
+/*Your first function! The function is declared with the word function it passes in two strings as parameters a & b.
+This function is pure which ensures it does not modify the state or the strings that are passed into it in the future
+The word internal means that this function can only be accessed within the contract.  We are returning a true or false or a
+bool */
+ 
+    	//TO DO: add a for loop to your function
+ 
+    	/* The for loop has 3 arguments. They are separated by a ;
+    	1. The first is the starting value the unsigned int is = to 0.
+    	2. the second (i < 6) is a test statement. Until this is no longer true the loop will execute
+    	3. the final statement is adding 1 to i with ++ which starts at 0. The loop will execute 6 times.
+    	One for each of the previously declared bytes */
+ 
+    	/* the if statement will check if a is not equal to b with the != operator. The index(or total string) of
+    	a and b is passed in with [i] and will return false*/
+         
+ 
+       //otherwise it will return true
+    
+ 	//TO: add events here. 3 of them with parameters passed in including address index(es) price quantity timestamp
+ 	//buyer seller if applicable
+ 
+  /* TO DO: Write a function that gets an asset index in the mapping by id
+  -the function will be public is used because we are storing some of this into block chain
+  -View is used here because it does not modify the state*/
+  
+ 	//TO DO: write a for loop that references asset count. I starts at 1 rather than the zero index      
+     
+ 	//TO: add an if statement to check the id and if it is correct return the inedec
+         
+ 	//If it is not return -1 for false this ends the function
+   
+ 
+	// TO DO: write a function that returns assets count
+ 
+	//TO DO: write a function that returns
+   
+   //TO DO: write a function that registers an asset into the assets list
+ 
+   /* This function registers an asset into the assets list(it is payable because it allows ether to be added to the network)
+   it is public because we need to access blockchain and modify the available assets.
+   if else statement will be sucheck if this a new asset or if we are updating an assets price and quantity. We are emitting the
+   events for either case with the emit statement.
+   */
+ 
+    	//get the real number using asset_count
+          
+    	//the memory keyword is used here because this is not being saved to blockchain
+        
+    	//count
+  	   
+    	//update blockchain with all in. msg.sender represents the address of the current user
+         
+    	//if we already have that id in place we will change the quantity and price   
+         
+        
+	// TO DO: write a function that gets an asset by index in the list
+  
+	//TO DO: Gets an asset by id (string)
+ 
+ 
+ 
+	//TO DO: write a function is declared the  source, target, quantity are parameters public is the visibility
+  
+    	//if there is no target or source return false   
+       
+    	//else
+    
+    	//add a new transaction to the transaction count    
+         
+    	//if statement emits if there are enough stock and update the assets accordingly
+            	// validate transaction
+            	//set a new quantity
+             
+            	//tell the count this worked
+               
+            	//update logs with transaction and asset information
+              
+        	// validate partial transaction  where only some of the stocks wanted were sold- still a transaction and the remainder is rejected
+           
+            	// create the rejected transaction
+               
+               
+        	//otherwise the transaction is rejected but still executed
+         
+    	//return true
+    
+ 
